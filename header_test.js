a9feb0f2da9701722843fc36f0adf5a36b0d0917
SVorokhobina <75305558+SVorokhobina@users.noreply.github.com>
2021-01-07 17:25:56 +1300

    Add files via upload
    Arrow icon toggles when the dropdown menu opens and hides.

--- header_test.js
+++ header_test.js
@@ -1,15 +1,28 @@
 function toggleMenu() {
   document.getElementById('toggling_elem').classList.toggle('showMenu');
+  
+  let x = document.getElementsByClassName("dropdown-content");
+  if (x[0].classList.contains('showMenu') == true) {
+      document.getElementById('icon_arrow').src = 'arrow_up.svg';
+	  document.getElementById('account_name').classList.add('dropdown_open_style');
+  } else {
+	  document.getElementById('icon_arrow').src = 'arrow_down.svg';
+	  document.getElementById('account_name').classList.remove('dropdown_open_style');
+  }
 }
+
 
 // To close the menu, if we click outside of it.
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbutton')) {
-    let x = document.getElementsByClassName("dropdown-content");
-    for (let i = 0; i < x.length; i++) {
-      if (x[i].classList.contains('showMenu') == true) {
-        x[i].classList.remove('showMenu');
-      }
+    if (!event.target.matches('.dropbutton')) {
+        let x = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < x.length; i++) {
+            if (x[i].classList.contains('showMenu') == true) {
+                x[i].classList.remove('showMenu');
+		        document.getElementById('icon_arrow').src = 'arrow_down.svg';
+		        document.getElementById('account_name').classList.remove('dropdown_open_style');
+            }
+        }
     }
-  }
 }
+
